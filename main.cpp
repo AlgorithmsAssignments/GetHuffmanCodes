d41ad85a81772a02a85a255c920a2fe255480f3f
Turupawn <ahmed.hn.43@gmail.com>
2017-02-22 17:00:24 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+map<char,string> getHuffmanCodes(BinaryNode* huffman_tree)
+{
+    map<char,string>answer;
+    return answer;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
